1d0c7daf2fbdd3a4d729102c473aae71cefaaaba
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2021-08-29 06:13:26 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,22 @@ $(document).ready(function () {
                 "background-color": "rgba(59, 59, 59, 0)",
             });
             // console.log("hide");
+        } else {
+            $(".navbar").css({
+                "margin-top": "0px",
+                opacity: "1",
+            });
+            $(".navbar-default").css({
+                "background-color": "rgba(59, 59, 59, 0.7)",
+                "border-color": "#444",
+            });
+            $(".navbar-brand img").css({
+                height: "35px",
+                "padding-top": "0px",
+            });
+            $(".navbar-nav > li > a").css({
+                "padding-top": "15px",
+            });
         }
     });
 });
